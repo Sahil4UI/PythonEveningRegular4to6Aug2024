231eb1430e05a25566116e33b00c1d72be5c4859
Sahil Kumar <54834362+Sahil4UI@users.noreply.github.com>
2024-08-09 18:00:35 +0530

    Update For Loop Patterns.py

--- For Loop Patterns.py
+++ For Loop Patterns.py
@@ -38,12 +38,29 @@ for i in range(1,6):
 ****
 *****
 '''
+'''
 for row in range(1,6):
     for col in range(row):
         print("*",end="")
     print()
+'''
+'''
+1
+12
+123
+1234
+12345
 
-
+1
+22
+333
+4444
+55555
+'''
+for row in range(1,6):
+    for col in range(row):
+        print(row,end="")
+    print()
 
 
     
